bd82fc117dcf44ea8e3d5e3e33aa07e0339cf174
Digital-Fortress-Institute <platform@digitalfortressltd.com>
2024-04-26 17:33:36 -0700

    my class

---
+++ class.py
@@ -0,0 +1,427 @@
+# print("Hello world!")
+# x = 5
+# y = 10
+# print(x + y)
+
+# first_name = input("Enter your first name \n")
+# second_name =  input("Enter your second name \n")
+# print(first_name + second_name )
+# concartination
+# print("my name is"+ " " + first_name+ " " + second_name)
+# print(f"My name is {first_name} {second_name}")
+
+# num1 = float(input("Enter your first number \n"))
+# num2 = float(input("Enter your second number \n"))
+# olamidebaddo =int(num1)
+# olamidebaddo2 = int(num2)
+# print(type(num1))
+# print(type(num2))
+
+# print(olamidebaddo + olamidebaddo2)
+
+# print(f"{num1} + {num2} = {num1 + num2}")
+
+
+# my_name = list()
+# print(type(my_name))
+
+# assignment solution
+# first_name=input("Enter your first name")
+# last_name=input("Enter your last name")
+# print(f" Mr/Mrs/Miss {first_name} {last_name} you are welcome")
+
+# num1=int(input("Enter your first number:  \n"))
+# num2=int(input("Enter your second number:  \n"))
+# print(f'{num1} + {num2} = {num1 + num2}')
+
+# my_name=input("enter your name")
+# my_year = int(input("Enter your year of birth"))
+# print(f"my nabe is {my_name} and I'm  {2024 - my_year} years old")
+
+
+# casting
+
+# my_name=float()
+# print(type(my_name))
+# my_string="uUniversity RoadHerbert Macualy 'Way'Akoka Yaba Lagos State Nigeria"
+# new_string='1234'
+
+# print(my_string.count("e"))
+# num= 10
+# num2 = 25
+# print(num + num2)
+# # bool
+# print(num < num2)
+# print(num > num2)
+# print(num == num2)
+
+# colors = ['blue', 'green', 'red', 'purple']
+# for tunde in colors:
+#     print(tunde[2])
+# colors[1]
+# print(colors)
+# for emeka in range(0, 11, 2):
+#     print(emeka)
+
+
+# employees=[
+#     {
+#         "name":'prosper',
+#         "occupation": ["python developer", "Data science"]
+#     },
+
+# ]
+# employees[0]['occupation']="Software dveloper"
+# print(employees)"banana"
+
+
+# myname = list()
+# my_list= ["apple", "orange", "banana"]
+# # my_list.sort()
+# # print(my_list)
+# newlist = []
+# # nwelist=my_list[2]
+# print(nwelist)
+# my_list.append("mango")
+# print(my_list)
+# my_list.remove("banana")
+# my_list.pop(2)
+# print(my_list)
+# newlist.extend(my_list)
+# print(newlist)
+# newlist = my_list == "banana"
+# print(newlist)
+
+# for i in my_list:
+#     if "mango" in i:
+#         print(True)
+#     else:
+#         print(False)
+
+# my_list = ["a", 'b', "c"]
+# my_list.reverse()
+# print(my_list)
+# print(len(my_list))
+
+
+# my_number = [ 2, 3, 9, 10, 0.1, 0.8, 0.9]
+# tunde = [x for x in my_number if x <= 2]
+# print(tunde)
+
+
+# print(tunde)
+# for i in my_number:
+#     print(i ** 2)
+# new_string = "Python class"
+# my_string = "suyfgsrtywwruowryr"
+
+# print(my_string.isdigit())
+# print(my_string())
+# print(my_string[::-1])
+# tunde=my_string.split()
+# tunde.reverse()
+# print(tunde)
+# for i in tunde:
+#     print(i)
+# print(my_string.find('to'))
+
+# if "fortress" in my_string:
+#     print('true')
+# else:
+#     print('False')
+# print(my_string+ ' ' + new_string)
+# print(f'{my_string} {new_string}')
+
+
+
+
+
+# my_list=list()
+# print(type(my_list))
+
+# my_tuple= ("Monday", "Tuesday", "wednesday", "Thursday", "Friday")
+# x=list(my_tuple)
+# # my_tuple[2]="Saturday"
+# x[0]="Sunday"
+# x.append("mARCH")
+# y=tuple(x)
+# print(y)
+
+
+# mytuple = ()
+# if mytuple:
+#     print("True")
+# else:
+#     print("False")
+
+# tunde =("blue", "red", "pink")
+# print(tunde)
+# print(len(tunde))
+# x = list(tunde)
+# tunde = (x[::-1])
+# print(tunde)
+# x = ("t3s0", 1, 2, 3, 4)
+# y = reversed(x)
+# print(tuple(y))
+# z = list(x)
+# u = tuple(z)
+# z.reverse()
+# u = tuple(z)
+# print(u)
+# mytuple = (2, 4, 6, 7, 10, "Hello")
+# index=mytuple.index(6)
+# print(index)
+# t = (3, 2, 8, 5)
+# color = ("blue", "orange")
+# # print(color)
+# tunde = list(color)
+# tunde.remove("orange")
+# color = tuple(tunde)
+# print(color)
+# if 7 in t:
+#     print("yes")
+# else:
+#     print("no")
+# print("tuple:, t")
+# print("maximum element:,  maximum_element")
+# print("minimum element:,  minimum_element")
+
+
+
+# operators
+# arithmetical operators - + * / % += -= ** >= <= == 
+
+num1 = 30
+num2 = 100
+num3 =120
+# print(num1 + num2)
+# print(num1 / num2)
+# print(num1 * num2)
+# print(num1 == num2)
+# print(num1 >= num2)
+# print(num1 <= num2)
+# print(num2 % num1% num3)
+# print(num1 ** 2)
+
+
+# logical operator and or 
+# country = "Nigeria"
+# age = 18
+
+# if country == "Nigeria" and age < 18:
+#     print("You can vote")
+# else:
+#     print('you can not vote')
+
+# score = input("Enter your score:   \n")
+# score = score.upper()
+# if score == "A" or score == "a":
+#     print('First position')
+# elif score == "B":
+#     print("Second position")
+# elif score == "C":
+#     print("Third Position")
+# elif score == "D":
+#     print("Fail")
+# else:
+#     print("Unknown Result")
+
+# loops
+x=0
+# while x < 11:
+#     print(x)
+#     x += 1
+
+# for i in range(0, 12):
+#     print(i)
+#     i += 1
+# import random
+# x = random.randint(0, 5)
+# print(x)
+
+# score = {10, 30, 25, 20, 60, 20, 10}
+# u = "Hello"
+# score.add(u)
+# print(score)
+# for x in score:
+#     if x > 20:
+#         print(x)
+#     else:
+#         print('')
+
+    # num = random.randint(1, 20)
+# num= range(0, 20)
+# for i in num:
+#     if i % 3 == 0:
+#         print("fizz")
+
+#     elif  i % 2 == 0:
+#         print("buzz")
+#     else:
+#         print(i)
+    
+# x = 1
+# while x <= 20:
+#     if x % 3 == 0:
+#         print("fizz")
+
+#     elif x % 2 == 0:
+#         print("buzz")
+#     else:
+#         print(x)
+#     x += 1
+
+# print("Tou are welcome")
+# a = input("Kindly type your desired number:  \n")
+# b = int(a)
+
+# if b > 0:
+#     print("positive")
+# elif b < 0:
+#     print("negative")
+# else:
+    
+#     print("zero")
+
+# num = int(input("Enter your number:"))
+# for mul in range(1, 20):
+#     # print("{0} * {1} = {2}".format(num, mul(num * mul)))
+#     print(f"{num} x {mul} = {num * mul}")
+
+# i = 1 
+# while i <= 10:
+#     print(num, "x", i, "=", num * i )
+#     i += 1
+
+# first_number = int(input("Enter your first number :"))
+# second_number =  int(input(":enter your second number"))
+# third_number =  int(input(":enter your third number"))
+
+# if (first_number > second_number) and (first_number > third_number):
+#     print("Your first is tyhe largest")
+
+# elif ( second_number > first_number ) and  (second_number >  third_number):
+#     print("Your second is tyhe largest")
+
+# else:
+#     print("Your third is tyhe largest")
+
+
+
+# if first_number > second_number:
+#     if first_number > third_number:
+#         largest = first_number
+#         print(f"{largest} first number is large")
+#     else:
+#         largest = third_number
+     
+#         if second_number > third_number:
+#             largest = second_number
+#             print(f"{largest} second number is the largest")
+# else:
+#     print(f"{largest} third number is larger")
+
+
+# grade=int(input("Enter your grade"))
+# if grade >= 90 and grade <= 100:
+#     print('grad A')
+# elif grade >= 70 and grade <= 89:
+#     print('Grade B')
+# elif grade >= 50 and grade <= 60:
+#     print(" grade C ")
+# elif grade >= 40 and grade <= 49:
+#     print("Fail")
+# else:
+#     print('Unkown results')
+# salary = float(input("Enter nyour salary: "))
+# if salary <= 5000:
+#     print(salary * 0.1) 
+# elif salary >= 5001 <= 10000:
+#     print(salary * 0.2) 
+# else:
+#     print(salary * 0.3) 
+   
+# num1 = 50
+# num2 = 60
+# num = num1 + num2 
+# if num> 100:
+#     print('The is greater than 100')
+# else:
+#     print("Sum is not greate than 100")
+
+# a = int(input("Enter age"))
+# if a > 60:
+#     print("you are eligible senior citizen discount")
+# else:
+#     ("print you are not eligible for citizen discount")
+# 
+# import os
+# os.system("cls" if os.name == "nt" else "clear")
+
+# while True:
+#     user_num = int(input("Enter ypor number: "))
+#     numbers = range(0, 4)
+#     if user_num in numbers:
+#         print("You are right congratulation")
+#         break
+#     else:
+#         print("Try Again")
+
+# import random
+# secret_number = random.randint(1, 3)
+# while True:
+#     guess = int(input("Guess the number (between 1 and 100):"))
+#     if guess == secret_number:
+#         print("Congratulations! you guess the wright number")
+#         break
+#     else:
+#         print("Wrong Guess. Try Again.  ")
+    
+# import random
+
+# while True:
+#     secret_number = random.randint(1, 3)
+#     guess = int(input("Guess the number (between 1 and 100):"))
+#     if guess == secret_number:
+#         print("Congratulations! you guess the wright number")
+#         break
+#     else:
+#         print("Wrong Guess. Try Again.  ")
+    
+
+word = ["Training", "Mango", "Tech", "Rock", "technicial", "software", "Analysis", "togo"
+"Tonga" ]
+
+# filtered=[i for i in word if i.startswith("T") or i.startswith('t')]
+# print(filtered)
+
+# for x in word:
+#     if x.startswith('T') or x.startswith('t'):
+#         print(x) 
+# 1. bring out the word that start with both capital letter T and small letter t
+
+person = [
+{
+"name":"Emeka",
+"email":"emeka@gmail.com",
+"country":["United States", "Canada", "Thailand", ["Ghana", "Morocco", "Germany"]]
+},
+{
+"name":"John",
+"email":"emeka@gmail.com",
+"country":["South Africa", "Congo", "", ["Senegal", ["Digital", ["Fortress"], "Software"],"China", "Data"]]
+},
+{
+"name":"Joy",
+"email":"emeka@gmail.com",
+"country":"Nigeria"
+},
+]
+print("i.", person[0]['country'][3][0])
+print('ii.',person[1]['country'][3][1][0] )
+print('iii.', person[1]['country'][3][1][1][0])
+
+# from the above data:
+# i. bring print out Ghana
+# ii. print out Digital
+# iii. print out fortress
